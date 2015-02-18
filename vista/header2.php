bccac16a06b86805758b0ffb4a86111dc57b1713
nacholozano <nacholozanogui@gmail.com>
2015-02-18 12:31:10 +0100

    Refresh size

--- vista/header2.php
+++ vista/header2.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="header2 col-md-12">
         <header class="col-md-offset-1">
-            <h3>MyCloud <span class="porcentaje"></span></h3>
+            <h3>MyCloud <span class="porcentaje"><?php include_once 'function/calcSize.php' ?></span></h3>
         </header>
     </div>
 </div>
