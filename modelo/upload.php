1d3849f16ac7295287f93788022b2f981acb73d1
nacholozano <nacholozanogui@gmail.com>
2015-02-20 23:10:15 +0100

    Modify $tmp creation

--- modelo/upload.php
+++ modelo/upload.php
@@ -1,8 +1,15 @@
 <?php
+
+include_once '../conf-app.php';
+
+if( !is_dir($tmp) ){
+    mkdir($tmp);
+}
+
 if(is_array($_FILES)) {
     if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {
         $sourcePath = $_FILES['userImage']['tmp_name'];
-        $targetPath = "/var/www/tmp/".$_FILES['userImage']['name'];
+        $targetPath = $tmp.$_FILES['userImage']['name'];
         if(move_uploaded_file($sourcePath,$targetPath)) {
 ?>
             <img src="<?php echo $targetPath; ?>" width="100px" height="100px" />
