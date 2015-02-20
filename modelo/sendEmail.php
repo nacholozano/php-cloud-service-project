9e35df10950020907214a41807c6a34f9a9c79fa
nacholozano <nacholozanogui@gmail.com>
2015-02-20 19:41:19 +0100

    Send a pdf with image to your email adress

--- modelo/sendEmail.php
+++ modelo/sendEmail.php
@@ -1,15 +1,13 @@
 <?php
 
-error_reporting(E_ALL);
-
 session_start();
 include_once '../conf-app.php';
 include_once '../otros/PHPMailer-master/PHPMailerAutoload.php';
 include_once '../mpdf60/mpdf.php';
 
-$toSend = $_GET["nombrePDF"];
+$pdfName = $_GET["nombrePDF"];
 $imageName = $_GET['inputImage'];
-$pdfToOpen = $rutaUsers."users/".$_SESSION["user"]."/pdf/".$toSend;
+$pdfToOpen = $rutaUsers."users/".$_SESSION["user"]."/pdf/".$pdfName;
 
 /* ---------- Añadir imagen al PDF ---------- */
 $mpdf = new mPDF();
@@ -22,10 +20,10 @@ $template = $mpdf->ImportPage($pdfFile);
 $mpdf->UseTemplate($template);
 $mpdf->AddPage();
 $mpdf->SetHTMLFooter ("<img src='".$tmp.$imageName."'/>");
-$mpdf->Output($tmp.$toSend ,"F");
+$mpdf->Output($tmp.$pdfName ,"F");
 
 /* ---------- Enviar PDF ---------- */
-/*
+
 $mail = new PHPMailer();
 //indico a la clase que use SMTP
 $mail->IsSMTP();
@@ -49,19 +47,17 @@ $mail->SetFrom("mycloud.hosting.php@gmail.com","MyCloud");
 /*
 $mail->AddReplyTo("tu_correo_electronico_gmail@gmail.com","Nombre completo");
 */
-/*
+
 $mail->Subject = "PDF MyCloud";
 $mail->MsgHTML("Aquí tienes tu PDF");
-$mail->AddAttachment(/*$rutaUsers."users/".$_SESSION["user"]."/pdf/".$toSend*//*$tmp.$toSend);*/
-/*
+$mail->AddAttachment($tmp.$pdfName);
+
 //indico destinatario
 $address = $_SESSION["user"];
 $mail->AddAddress($address, "Nombre completo");
+$mail->Send();
 
-if(!$mail->Send()) {
-    echo "Error al enviar: " . $mail->ErrorInfo;
-} else {
-    echo "Mensaje enviado!";
-}
-*/
+unlink($tmp.$pdfName);
+unlink($tmp.$imageName);
+
 ?>
