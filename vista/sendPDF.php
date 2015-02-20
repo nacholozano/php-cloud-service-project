627d50062fd6355730680ce90ee60bece60b047f
nacholozano <nacholozanogui@gmail.com>
2015-02-20 12:56:49 +0100

    Change input style

--- vista/sendPDF.php
+++ vista/sendPDF.php
@@ -2,14 +2,14 @@
 
 <div class="anadirImagen">
         <span class="spanCerrar glyphicon glyphicon-remove"></span>
-        <div class="Imagen input-group">
-           <form id="uploadForm" action="modelo/upload.php" method="post">
+            <form id="uploadForm" action="modelo/upload.php" method="post">
+              <div class="Imagen input-group">
                 <input type="file" id="inputImage" name="userImage" class="form-control">
                 <span class="input-group-btn">
                     <button type="submit" value="submit" id="boton-enviarCorreo" name="" class="btn btn-default">Enviar</button>
                 </span>
+              </div>
             </form>
-        </div>
         <div id="divPreview">
             <img src="" id="preview" alt="">
             <p class="alert alert-danger" role='alert' id="notImage">No has seleccionado ninguna imagen</p>
